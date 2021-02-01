d9f7288e98b0d95dc1992867512139517f34e08e
Panda64 <JaylenSchelb@yahoo.com>
2021-01-31 17:25:31 -0800

    Completed Assignment
    Have yet to fix import errors.

--- events_app/models.py
+++ events_app/models.py
@@ -1,6 +1,14 @@
 """Create database models to represent tables."""
 from events_app import db
 from sqlalchemy.orm import backref
+import enum
+
+
+class EventType(enum.Enum):
+    PARTY = 1
+    STUDY = 2
+    NETWORKING = 3
+    GENERAL = 4
 
 # TODO: Create a model called `Guest` with the following fields:
 # - id: primary key
@@ -11,6 +19,11 @@ from sqlalchemy.orm import backref
 
 class Guest(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(80), nullable=False)
+    phone = db.Column(db.String(80), nullable=False)
+
+    events_attending = db.relationship('Event', secondary="guest_event", back_populates="guest")
 
 # TODO: Create a model called `Event` with the following fields:
 # - id: primary key
@@ -24,9 +37,19 @@ class Guest(db.Model):
 
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(80), nullable=False)
+    date_and_time = db.Column(db.Date)
+
+    guest = db.relationship('Guest', secondary="guest_event", back_populates="events_attending")
+
+    event_type = db.Column(db.Enum(EventType), default=EventType.GENERAL)
 
 # TODO: Create a table `guest_event_table` with the following columns:
 # - book_id: Integer column (foreign key)
 # - genre_id: Integer column (foreign key)
 
-guest_event_table = None
+guest_event_table = db.Table('guest_event',
+    db.Column('guest_id', db.Integer, db.ForeignKey('guest.id')),
+    db.Column('event_id', db.Integer, db.ForeignKey('event.id'))
+)
--- events_app/routes.py
+++ events_app/routes.py
@@ -18,14 +18,16 @@ main = Blueprint('main', __name__)
 def index():
     """Show upcoming events to users!"""
     # TODO: Get all events and send to the template
-    return render_template('index.html')
+    events = Event.query.all()
+    return render_template('index.html', events=events)
 
 
 @main.route('/event/<event_id>', methods=['GET'])
 def event_detail(event_id):
     """Show a single event."""
     # TODO: Get the event with the given id and send to the template
-    return render_template('event_detail.html')
+    event = Event.query.get(event_id)
+    return render_template('event_detail.html', event=event)
 
 
 @main.route('/event/<event_id>', methods=['POST'])
@@ -34,17 +36,26 @@ def rsvp(event_id):
     # TODO: Get the event with the given id from the database
     is_returning_guest = request.form.get('returning')
     guest_name = request.form.get('guest_name')
+    event = Event.query.get(event_id)
 
     if is_returning_guest:
         # TODO: Look up the guest by name, and add the event to their 
         # events_attending, then commit to the database
-        pass
+        guest = Guest.query.filter_by(name=guest_name).one()
+        guest.events_attending.append(event)
+
+        db.session.add(guest)
+        db.session.commit()
     else:
         guest_email = request.form.get('email')
         guest_phone = request.form.get('phone')
         # TODO: Create a new guest with the given name, email, and phone, and 
         # add the event to their events_attending, then commit to the database
-        pass
+        guest = Guest(name=guest_name, email=guest_email, phone=guest_phone)
+        guest.events_attending.append(event)
+
+        db.session.add(guest)
+        db.session.commit()
     
     flash('You have successfully RSVP\'d! See you there!')
     return redirect(url_for('main.event_detail', event_id=event_id))
@@ -68,6 +79,10 @@ def create():
 
         # TODO: Create a new event with the given title, description, & 
         # datetime, then add and commit to the database
+        event = Event(title=new_event_title, description=new_event_description, date_and_time=date_and_time)
+
+        db.session.add(event)
+        db.session.commit()
 
         flash('Event created.')
         return redirect(url_for('main.homepage'))
@@ -78,4 +93,5 @@ def create():
 @main.route('/guest/<guest_id>')
 def guest_detail(guest_id):
     # TODO: Get the guest with the given id and send to the template
-    return render_template('guest_detail.html')
+    guest = Guest.query.get(guest_id)
+    return render_template('guest_detail.html', guest=guest)
